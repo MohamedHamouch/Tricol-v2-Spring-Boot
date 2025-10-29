2807606b86cb472e180dcadb9a2541d500b0f46a
Mohamed Hamouch <med10hamouch@gmail.com>
2025-10-29 16:22:14 +0100

    create OrderProduct entity

---
+++ src/main/java/ma/tricol/supplies/model/OrderProduct.java
@@ -0,0 +1,34 @@
+package ma.tricol.supplies.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_product")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private SupplierOrder order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(name = "unit_price", nullable = false)
+    private BigDecimal unitPrice;
+}
