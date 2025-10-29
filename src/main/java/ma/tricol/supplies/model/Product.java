1ae1e365774de6a95d9e14f7d852b87b04291943
Mohamed Hamouch <med10hamouch@gmail.com>
2025-10-29 15:27:18 +0100

    create Product entity

--- src/main/java/ma/tricol/supplies/model/Product.java
+++ src/main/java/ma/tricol/supplies/model/Product.java
@@ -1,22 +1,34 @@
 package ma.tricol.supplies.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Entity
+@Table(name = "product")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-@Table(name = "products")
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
+
+    @Column(columnDefinition = "TEXT")
     private String description;
-    private double unitPrice;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
     private String category;
+
+    @Column(name = "current_stock")
+    private Integer currentStock = 0;
 }
