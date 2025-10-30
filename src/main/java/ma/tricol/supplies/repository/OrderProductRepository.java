adea9ddc92c248c0c7a95b61b61c591da17fecd0
Mohamed Hamouch <med10hamouch@gmail.com>
2025-10-30 11:05:42 +0100

    Add OrderProductRepository

---
+++ src/main/java/ma/tricol/supplies/repository/OrderProductRepository.java
@@ -0,0 +1,9 @@
+package ma.tricol.supplies.repository;
+
+import ma.tricol.supplies.model.OrderProduct;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
+}
