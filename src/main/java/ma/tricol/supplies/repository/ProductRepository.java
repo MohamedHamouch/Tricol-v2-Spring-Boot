a312a233071d349deed191a8184506cf3060def3
Mohamed Hamouch <med10hamouch@gmail.com>
2025-10-30 09:47:51 +0100

    Add ProductRepository

---
+++ src/main/java/ma/tricol/supplies/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package ma.tricol.supplies.repository;
+
+import ma.tricol.supplies.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
