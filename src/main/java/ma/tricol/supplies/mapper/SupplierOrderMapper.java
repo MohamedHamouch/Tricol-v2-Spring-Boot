7144121fbaa7be81f4aa6c77e78ef5774ec086c2
Mohamed Hamouch <med10hamouch@gmail.com>
2025-10-31 17:09:12 +0100

    Add SupplierOrderMapper

---
+++ src/main/java/ma/tricol/supplies/mapper/SupplierOrderMapper.java
@@ -0,0 +1,18 @@
+﻿package ma.tricol.supplies.mapper;
+
+import ma.tricol.supplies.dto.SupplierOrderDTO;
+import ma.tricol.supplies.model.SupplierOrder;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = {OrderProductMapper.class})
+public interface SupplierOrderMapper {
+    
+    @Mapping(source = "supplier.id", target = "supplierId")
+    @Mapping(source = "supplier.company", target = "supplierCompany")
+    SupplierOrderDTO toDTO(SupplierOrder order);
+    
+    @Mapping(source = "supplierId", target = "supplier.id")
+    @Mapping(target = "orderProducts", ignore = true)
+    SupplierOrder toEntity(SupplierOrderDTO dto);
+}
