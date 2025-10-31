30815d3eea8d70f7e47a5533b1793b85faaf3d88
Mohamed Hamouch <med10hamouch@gmail.com>
2025-10-31 16:47:33 +0100

    Add OrderProductMapper

---
+++ src/main/java/ma/tricol/supplies/mapper/OrderProductMapper.java
@@ -0,0 +1,19 @@
+﻿package ma.tricol.supplies.mapper;
+
+import ma.tricol.supplies.dto.OrderProductDTO;
+import ma.tricol.supplies.model.OrderProduct;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface OrderProductMapper {
+    
+    @Mapping(source = "order.id", target = "orderId")
+    @Mapping(source = "product.id", target = "productId")
+    @Mapping(source = "product.name", target = "productName")
+    OrderProductDTO toDTO(OrderProduct orderProduct);
+    
+    @Mapping(source = "orderId", target = "order.id")
+    @Mapping(source = "productId", target = "product.id")
+    OrderProduct toEntity(OrderProductDTO dto);
+}
