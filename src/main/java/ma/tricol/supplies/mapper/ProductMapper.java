885273e4133dc28f7fb18f645174e9eaf36d1268
Mohamed Hamouch <med10hamouch@gmail.com>
2025-10-31 16:18:45 +0100

    Add ProductMapper

---
+++ src/main/java/ma/tricol/supplies/mapper/ProductMapper.java
@@ -0,0 +1,13 @@
+﻿package ma.tricol.supplies.mapper;
+
+import ma.tricol.supplies.dto.ProductDTO;
+import ma.tricol.supplies.model.Product;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+    
+    ProductDTO toDTO(Product product);
+    
+    Product toEntity(ProductDTO dto);
+}
