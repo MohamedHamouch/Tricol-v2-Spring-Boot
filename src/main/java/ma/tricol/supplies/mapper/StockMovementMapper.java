8433302172aa7ca1fc48987c2e400f94b5f620ab
Mohamed Hamouch <med10hamouch@gmail.com>
2025-10-31 17:28:55 +0100

    Add StockMovementMapper

---
+++ src/main/java/ma/tricol/supplies/mapper/StockMovementMapper.java
@@ -0,0 +1,19 @@
+﻿package ma.tricol.supplies.mapper;
+
+import ma.tricol.supplies.dto.StockMovementDTO;
+import ma.tricol.supplies.model.StockMovement;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface StockMovementMapper {
+    
+    @Mapping(source = "product.id", target = "productId")
+    @Mapping(source = "product.name", target = "productName")
+    @Mapping(source = "order.id", target = "orderId")
+    StockMovementDTO toDTO(StockMovement movement);
+    
+    @Mapping(source = "productId", target = "product.id")
+    @Mapping(source = "orderId", target = "order.id")
+    StockMovement toEntity(StockMovementDTO dto);
+}
