33c9045a6aa9d605c29a821e0cacda1c2308dd06
Mohamed Hamouch <med10hamouch@gmail.com>
2025-10-31 15:52:19 +0100

    Add SupplierMapper with MapStruct

---
+++ src/main/java/ma/tricol/supplies/mapper/SupplierMapper.java
@@ -0,0 +1,13 @@
+﻿package ma.tricol.supplies.mapper;
+
+import ma.tricol.supplies.dto.SupplierDTO;
+import ma.tricol.supplies.model.Supplier;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface SupplierMapper {
+    
+    SupplierDTO toDTO(Supplier supplier);
+    
+    Supplier toEntity(SupplierDTO dto);
+}
